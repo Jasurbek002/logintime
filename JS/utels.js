3af8043a8ed76b38f78429fb5932b41cdb75c338
unknown <jasurbekturgunov25@gmail.com>
2022-02-20 03:34:49 +0500

    togrilandi

--- JS/utels.js
+++ JS/utels.js
@@ -1,10 +1,10 @@
 let token =window.localStorage.getItem('token')
 
 if(!token){
-    window.location.replace('index.html')
+    window.location.replace('login.html')
     alert('Sizda token yoq')
 }
-if(!token==undefined){
-    window.location.replace('index.html')
-    alert('Sizda token yoq')
-}
+// if(!token==undefined){
+//     window.location.replace('index.html')
+//     alert('Sizda token yoq')
+// }
